d662ce8497cd0d4a3a99ed595d131fbfab78689a
Aurélien G <aureli1.gau@gmail.com>
2019-07-06 14:08:14 +0200

    first message found in the soup!

--- preparation.py
+++ preparation.py
@@ -3,9 +3,12 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 
 current_path = Path(__file__).parent
-file_path = (current_path / "./Data/messages2.html").resolve()
+file_path = (current_path / "./Data/messages.html").resolve()
 
 with open(file_path) as fp:
     soup = BeautifulSoup(fp)
 
+print(soup.div.div.prettify())
+
+print(soup.find(attrs={"class" : "message default clearfix"}).prettify())
 
